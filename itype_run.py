9e0dc6f679c4f93de6067b72d214a9b945ca4faa
pari23367 <155488182+pari23367@users.noreply.github.com>
2024-03-11 02:15:36 +0530

    Update itype_run.py

--- itype_run.py
+++ itype_run.py
@@ -23,6 +23,58 @@ def I_Type_Encoding(line):
     else:
         list1= line.split()
         list2= list1[1].split(',')
+
+
+
+or 
+def I_Type_Encoding(line):
+    global OPCODE
+    global Address
+
+    if halt_encountered:
+        print(f'ILLEGAL_COMMAND: HALT already encountered')
+        terminate()
+
+    # Move the assignment before the conditional statement
+    INSTRUCTION = line.split()[0]
+    OPCODE = OPCODES[INSTRUCTION]
+
+    if OPCODE == 'lw':
+        list1 = line.split()
+        op = list1[0]
+        desr = list1[1]
+
+        # Extract immediate value and register from the operand
+        im_sr = list1[2].rstrip(')').split(',')
+        im, sr = im_sr[0], im_sr[1]
+
+        list3 = [op, desr, im, sr]
+
+        FUNCT3 = funct3[INSTRUCTION]
+        imm = binary(list3[2])
+        rd = Register_Encoding[list3[1]]
+        rs1 = Register_Encoding[list3[3]]
+        binary_answer = imm + rs1 + FUNCT3 + rd + OPCODE  # Corrected order
+        print(binary_answer)
+
+    else:
+        list1 = line.split()
+        list2 = list1[1].split(',')
+        list3 = []
+        list3.append(list1[0])
+        for i in list2:
+            list3.append(i)
+
+        FUNCT3 = funct3[INSTRUCTION]
+        rd = Register_Encoding[list3[1]]
+        rs1 = Register_Encoding[list3[2].rstrip(',')]
+        imm = binary(list3[3])
+        binary_answer = imm + rs1 + FUNCT3 + rd + OPCODE  # Corrected order
+        print(binary_answer)
+
+
+I_Type_Encoding("lw a5,20(s1)")
+
         list3=[]
         list3.append(list1[0])
         for i in list2:
