c9e4d97dfee3f4779a06f79d855a74239b1a6dd1
Pragyasingh001 <162748229+Pragyasingh001@users.noreply.github.com>
2024-03-11 16:41:53 +0530

    Utype.py

--- utype.py
+++ utype.py
@@ -10,14 +10,14 @@ def U_Type_Encoding(line,location) :
         for i in list2:
             list3.append(i)
     #list3 first element is lui or auipc
-    #list3 2nd element is rd and 3rd element is the immediate valye
+    #list3 2nd element is rd and 3rd element is the immediate value
         no_error_in_register_name(list3[1],location)
         no_error_in_register_name(list3[2],location)
         no_error_in_register_name(list3[3],location)
 
         INSTRUCTION = list3[0]
 
-        if INSTRUCTION=="lui":  #check for the first element is auipc or lui 
+        if INSTRUCTION=="lui":  #check for the instruction if it is auipc or lui 
             OPCODE=OPCODES[INSTRUCTION]
             RD= Register_Encoding[list3[1]]
             IMM = list[2]
